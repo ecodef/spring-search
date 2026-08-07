f41adcaff1244a17eab7c5424909e215cbd0ded5
Michal Valach <75176355+michalvalach@users.noreply.github.com>
2026-08-07 14:02:10 +0200

    Fulltext search in enums (#13)

--- src/main/kotlin/com/sipios/springsearch/strategies/EnumStrategy.kt
+++ src/main/kotlin/com/sipios/springsearch/strategies/EnumStrategy.kt
@@ -1,11 +1,19 @@
 package com.sipios.springsearch.strategies
 
 import com.sipios.springsearch.SearchOperation
+import java.util.Locale
 import kotlin.reflect.KClass
 
 class EnumStrategy : ParsingStrategy {
     override fun parse(value: String?, fieldClass: KClass<out Any>): Any? {
         if (value == SearchOperation.NULL) return value
-        return Class.forName(fieldClass.qualifiedName).getMethod("valueOf", String::class.java).invoke(null, value)
+        return try {
+            Class
+                .forName(fieldClass.qualifiedName)
+                .getMethod("valueOf", String::class.java)
+                .invoke(null, value?.uppercase(Locale.getDefault()))
+        } catch (e: Exception) {
+            value?.uppercase(Locale.getDefault())
+        }
     }
 }
